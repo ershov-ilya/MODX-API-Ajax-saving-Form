9236065bf3d11d35a1b1d16595564aaad02682f1
ershov-ilya <ershov.ilya@gmail.com>
2015-02-24 15:41:51 +0300

    Изменения в анкете

--- referal/utm/index.php
+++ referal/utm/index.php
@@ -54,9 +54,13 @@ $res=false;
 while($res==false && $i>0) {
     try {
         extract($dbconfig);
-        $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-        $stmt = $db->prepare("INSERT INTO `gnewsbs_crm`.`referal_utm` (`utm`) VALUES (:utm);");
+        $db = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $stmt = $db->prepare("INSERT INTO `gnewsbs_crm`.`referal_utm` (utm, name, email, phone) VALUES (:utm, :name, :email, :phone);");
         $stmt->bindParam(':utm', $utm);
+        $stmt->bindParam(':name', $_POST['name']);
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->bindParam(':phone', $_POST['phone']);
         $res = $stmt->execute();
         if(!$res) {$utm=generateRandomString(12); $utm=$prefix.$utm;}
             $i--;
