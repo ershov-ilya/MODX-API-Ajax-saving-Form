2f1f65391bfd3d668b558c7b24c5856076004b4f
ershov-ilya <ershov.ilya@gmail.com>
2015-09-04 12:14:57 +0300

    рефакторинг student/census/update/ Saved successfully

--- student/census/update/index.php
+++ student/census/update/index.php
@@ -35,20 +35,30 @@ try {
 // Include classes
     require_once('../../../core/config/api.private.config.php');
     require_once(API_CORE_PATH . '/class/restful/restful.class.php');
-    $rest = new RESTful('update', 'verify,created,updated,name,secondname,patronymic,dob,gender,studgroup,affiliate,phone,email,contact1_name,contact1_phone,contact2_name,contact2_phone,contact3_name,contact3_phone,vk_id,interests,prof_experience,prof_plan,prof_orientation,prof_status,prof_income,referer,source,sourceId,http_referer');
+    $rest = new RESTful('update', 'verify,created,updated,name,secondname,patronymic,birth_year,birth_month,birth_day,gender,studgroup,affiliate,phone,email,contact1_name,contact1_phone,contact2_name,contact2_phone,contact3_name,contact3_phone,vk_id,interests,prof_experience,prof_plan,prof_orientation,prof_status,prof_income,referer,source,sourceId,http_referer');
 
     $hours3 = 60 * 60 * 3;
     $time = time() + $hours3;
 
     $prop = array(
         'updated' => $time,
-        'birth_day' => '1',
-        'birth_month' => '1',
-        'birth_year' => '1970'
     );
 
-    $prop = array_merge($prop, $_REQUEST);
-    $prop['dob'] = date_timestamp_get(date_create($prop['birth_year'] . "-" . $prop['birth_month'] . "-" . $prop['birth_day']));
+    // Преобразование даты рождения
+    if(isset($rest->data['birth_year']) || isset($rest->data['birth_month']) || isset($rest->data['birth_day'])) {
+        $bd=array(
+            'birth_day' => ($rest->data['birth_day'])?($rest->data['birth_day']):'1',
+            'birth_month' => ($rest->data['birth_month'])?($rest->data['birth_month']):'1',
+            'birth_year' => ($rest->data['birth_year'])?$rest->data['birth_year']:'1970'
+        );
+        $prop=array_merge($prop, $bd);
+        $prop['dob'] = date_timestamp_get(date_create($bd['birth_year'] . "-" . $bd['birth_month'] . "-" . $bd['birth_day']));
+    }
+
+    $prop = array_merge($rest->data,$prop);
+    unset($prop['birth_year']);
+    unset($prop['birth_month']);
+    unset($prop['birth_day']);
 
     //$modx->log(MODX_LOG_LEVEL_WARN,'DOB: '.$prop['dob']);
 
@@ -78,64 +88,80 @@ try {
         $response['status'] = 'failed';
         throw new Exception('Wrong verify',403);
    }
+    unset($prop['verify']);
 
     if (DEBUG) {
         print_r($rest->data);
-        print_r($data);
         print_r($prop);
-        throw new Exception('Stop',200);
+        print_r($data);
+        //throw new Exception('Stop',200);
     }
 
-    if ($response['status'] != 'failed') {
-        $object->set('updated', $prop['updated']);
 
-//        foreach($prop as $k=>$field){
-//            switch($field){
-//                case 'updated':
-//                    $object->set('created', $prop['updated']);
-//                    $object->set('updated', $prop['updated']);
-//                    break;
-//                default:
-//                    $object->set($k, $prop[$k]);
-//            }
-//        }
-
-        if (isset($prop['name'])) $object->set('name', $prop['name']);
-        if (isset($prop['secondname'])) $object->set('secondname', $prop['secondname']);
-        if (isset($prop['patronymic'])) $object->set('patronymic', $prop['patronymic']);
-        if (isset($prop['dob'])) $object->set('dob', $prop['dob']);
-        if (isset($prop['gender'])) $object->set('gender', $prop['gender']);
-        if (isset($prop['studgroup'])) $object->set('studgroup', $prop['studgroup']);
-        if (isset($prop['affiliate'])) $object->set('affiliate', $prop['affiliate']);
-        if (isset($prop['phone'])) $object->set('phone', $prop['phone']);
-        if (isset($prop['email'])) $object->set('email', $prop['email']);
-        if (isset($prop['contact1_name'])) $object->set('contact1_name', $prop['contact1_name']);
-        if (isset($prop['contact1_phone'])) $object->set('contact1_phone', $prop['contact1_phone']);
-        if (isset($prop['contact2_name'])) $object->set('contact2_name', $prop['contact2_name']);
-        if (isset($prop['contact2_phone'])) $object->set('contact2_phone', $prop['contact2_phone']);
-        if (isset($prop['contact3_name'])) $object->set('contact3_name', $prop['contact3_name']);
-        if (isset($prop['contact3_phone'])) $object->set('contact3_phone', $prop['contact3_phone']);
-        if (isset($prop['vk_id'])) $object->set('vk_id', $prop['vk_id']);
-        if (isset($prop['interests'])) $object->set('interests', serialize($prop['interests']));
-        if (isset($prop['prof_experience'])) $object->set('prof_experience', $prop['prof_experience']);
-        if (isset($prop['prof_plan'])) $object->set('prof_plan', $prop['prof_plan']);
-        if (isset($prop['prof_orientation'])) $object->set('prof_orientation', $prop['prof_orientation']);
-        if (isset($prop['prof_status'])) $object->set('prof_status', $prop['prof_status']);
-        if (isset($prop['prof_income'])) $object->set('prof_income', $prop['prof_income']);
-        if (isset($prop['referer'])) $object->set('referer', $prop['referer']);
-
-        if ($object->save()) $response['status'] = 'OK';
-        else  $response['status'] = 'failed';
-
-        $id = $object->get('id');
-
-        $response = array(
-            'status' => 'OK',
-            'data' => $data
-        );
-
-        $response['data']['id'] = $id;
+    foreach($prop as $k=>$v){
+        switch($k){
+            case 'source':
+                if(empty($data['source'])){
+                    $object->set('source', $prop['source']);
+                }
+                break;
+            case 'sourceId':
+                if(empty($data['sourceId'])){
+                    $object->set('sourceId', $prop['sourceId']);
+                }
+                break;
+            case 'interests':
+                $object->set('interests', serialize($prop['interests']));
+                break;
+            case 'updated':
+                $object->set('created', $prop['updated']);
+                $object->set('updated', $prop['updated']);
+                break;
+            default:
+                $object->set($k, $v);
+        }
     }
+
+//    $object->set('updated', $prop['updated']);
+//    if (isset($prop['name'])) $object->set('name', $prop['name']);
+//    if (isset($prop['secondname'])) $object->set('secondname', $prop['secondname']);
+//    if (isset($prop['patronymic'])) $object->set('patronymic', $prop['patronymic']);
+//    if (isset($prop['dob'])) $object->set('dob', $prop['dob']);
+//    if (isset($prop['gender'])) $object->set('gender', $prop['gender']);
+//    if (isset($prop['studgroup'])) $object->set('studgroup', $prop['studgroup']);
+//    if (isset($prop['affiliate'])) $object->set('affiliate', $prop['affiliate']);
+//    if (isset($prop['phone'])) $object->set('phone', $prop['phone']);
+//    if (isset($prop['email'])) $object->set('email', $prop['email']);
+//    if (isset($prop['contact1_name'])) $object->set('contact1_name', $prop['contact1_name']);
+//    if (isset($prop['contact1_phone'])) $object->set('contact1_phone', $prop['contact1_phone']);
+//    if (isset($prop['contact2_name'])) $object->set('contact2_name', $prop['contact2_name']);
+//    if (isset($prop['contact2_phone'])) $object->set('contact2_phone', $prop['contact2_phone']);
+//    if (isset($prop['contact3_name'])) $object->set('contact3_name', $prop['contact3_name']);
+//    if (isset($prop['contact3_phone'])) $object->set('contact3_phone', $prop['contact3_phone']);
+//    if (isset($prop['vk_id'])) $object->set('vk_id', $prop['vk_id']);
+//    if (isset($prop['interests'])) $object->set('interests', serialize($prop['interests']));
+//    if (isset($prop['prof_experience'])) $object->set('prof_experience', $prop['prof_experience']);
+//    if (isset($prop['prof_plan'])) $object->set('prof_plan', $prop['prof_plan']);
+//    if (isset($prop['prof_orientation'])) $object->set('prof_orientation', $prop['prof_orientation']);
+//    if (isset($prop['prof_status'])) $object->set('prof_status', $prop['prof_status']);
+//    if (isset($prop['prof_income'])) $object->set('prof_income', $prop['prof_income']);
+//    if (isset($prop['referer'])) $object->set('referer', $prop['referer']);
+
+    if ($object->save()) $response['status'] = 'OK';
+    else  {
+        $response['status'] = 'failed';
+        throw new Exception('Failed to save',500);
+    }
+
+    $id = $object->get('id');
+
+    $response = array(
+        'status' => 'OK',
+        'data' => $prop,
+        'message' => 'Saved successfully'
+    );
+
+    $response['data']['id'] = $id;
 }
 catch(Exception $e){
     $response['message']=$e->getMessage();
